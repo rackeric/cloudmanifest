66113d4303fcd079a42777dba0290be5b1affa65
Eric Hernandez <Eric Hernandez>
2015-11-08 19:18:22 -0600

    move stringio stuff inside if statement

--- manifest/tasks.py
+++ manifest/tasks.py
@@ -327,6 +327,8 @@ def run_ansible_playbook(user_id, project_id, playbook_id):
         #myExternalData.patch(playbook_id, {'stderr': myStderr})
     finally:
         print myStdout
+        print mtStderr
+        print play
     #    sys.stdout = prev
     #    sys.stderr = prev2
 
