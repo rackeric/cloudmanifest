b87f39e8c9afdc0fa672794eb58ed2ca372a62e6
Eric Hernandez <Eric Hernandez>
2015-11-09 18:03:36 -0600

    adding status on api job send attempt 3

--- manifest/static/js/controllers.js
+++ manifest/static/js/controllers.js
@@ -658,7 +658,8 @@ angular.module('myApp.controllers', [])
         if (runPlay) {
           // ansible role return results
           $scope.role_returns_status = syncData('users/' + $scope.auth.user.uid + '/projects/' + $scope.projectID + '/roles/' + playbook_key + '/status');
-          $scope.role_returns_status.$save("QUEUED");
+          $scope.role_returns_status = "QUEUED";
+          $scope.role_returns_status.$save();
           // add host to runPlayAlert
 	      $scope.runPlayAddAlert('success', 'Running playbook, if it does not complete check that hosts and user are set in the play.');
 
