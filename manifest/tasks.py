d4fe700fd4cd80aa186ee852899ff7355bcfa91f
Eric Hernandez <Eric Hernandez>
2015-11-11 18:41:31 -0600

    add conditional for playHosts and playUsername

--- manifest/tasks.py
+++ manifest/tasks.py
@@ -340,9 +340,9 @@ def run_ansible_playbook(user_id, project_id, playbook_id):
         myExternalData.post(playbook_id + '/returns', {'stats': sanitize_keys(play), 'stdout': convert_bash_colors(myStdout)})
         #myExternalData.patch(playbook_id, {'stderr': myStderr})
     finally:
-        print myStdout
-        print myStderr
-        print play
+        #print myStdout
+        #print myStderr
+        #print play
     #    sys.stdout = prev
     #    sys.stderr = prev2
 
