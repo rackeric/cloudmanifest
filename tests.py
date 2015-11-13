a0f4b60b275b0750b5e8ca21d08a22421956132c
Eric Hernandez <Eric Hernandez>
2015-11-13 09:49:37 -0600

    adding more tests

--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@ import os
 import manifest
 import unittest
 from manifest.tasks import convert_bash_colors
+from manifest.tasks import sanitize_keys
 
 class ManifestTestCase(unittest.TestCase):
 
@@ -18,10 +19,15 @@ class ManifestTestCase(unittest.TestCase):
     #    self.assertEqual(result.status_code, 200)
 
     def test_remove_bash_colors(self):
-        expect = "this is a string"
-        string = "this i[0;32ms a st[0mring"
+        string = "this i[0;32ms a string"
         result = convert_bash_colors(string)
-        assert expect in result
+        assert '[0;32m' not in result
+        string = "this i[0;32ms a string"
+
+    def test_sanitize_keys(self):
+        string = "192.168.1.1"
+        result = sanitize_keys(string)
+        self.assertEqual(result, "192_168_1_1")
 
 if __name__ == '__main__':
     unittest.main()
