2240443700fc807a10f719bd3a6fc38339f9d476
Eric Hernandez <Eric Hernandez>
2015-11-15 21:29:18 -0600

    getting playbooks on git project add

--- manifest/__init__.py
+++ manifest/__init__.py
@@ -43,7 +43,7 @@ app.add_url_rule('/api/v1/ansible_playbook_manual/<string:user_id>/<string:proje
 app.add_url_rule('/api/v1/rax_create_server/<string:user_id>/<string:project_id>/<string:job_id>',
                  view_func=RaxCreateServer.as_view('rax_create_server'))
 # api route for populating playbooks from git repo
-app.add_url_rule('/api/v1/populate_playbooks/<string:user_id>/<string:project_id>/<string:repo_id>',
+app.add_url_rule('/api/v1/populate_playbooks/<string:user_id>/<string:project_id>/<string:playbook_id>',
                  view_func=PopulatePlaybooks.as_view('populate_playbooks'))
 # api route for git projects ansible run
 app.add_url_rule('/api/v1/ansible_playbook_git/<string:user_id>/<string:project_id>/<string:playbook_id>',
