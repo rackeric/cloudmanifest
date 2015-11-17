e597d68282cf96f6e15b361f638c9f534aef8bb4
Eric Hernandez <Eric Hernandez>
2015-11-17 13:49:24 -0600

    add Firebase.ServerValue.TIMESTAMP to first method

--- manifest/tasks.py
+++ manifest/tasks.py
@@ -1,6 +1,6 @@
 from StringIO import StringIO
 from firebase import FirebaseApplication, FirebaseAuthentication
-from firebase import Firebase
+#from firebase import Firebase
 import firebase
 from celery.decorators import task
 from ansible import utils
@@ -805,7 +805,7 @@ def run_ansible_playbook_git(user_id, project_id, playbook_id, play_name):
     finally:
         sys.stdout = prev
         sys.stderr = prev2
-        myExternalData.put(playbook_id, {"endedAt": Firebase.ServerValue.TIMESTAMP})
+        myExternalData.post(playbook_id, {"endedAt": Firebase.ServerValue.TIMESTAMP})
 
 
 
