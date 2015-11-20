c55fb86bfba9c57158ad56537df8f8113a0b4073
Eric Hernandez <Eric Hernandez>
2015-11-19 22:46:01 -0600

    starting run_ansible_jeneric mock tests

--- tests.py
+++ tests.py
@@ -77,13 +77,12 @@ class ManifestTestCase(unittest.TestCase):
                    "module_args" : "",
                    "project_id" : "proj123",
                    "remote_pass" : "lkjlkj",
-                   "remote_user" : "root",
-                   # inventory options
-                   "name": "host1",
-                   "group": "group1",
-                   "ansible_ssh_host": "host1",
-                   "ansible_ssh_user": "root"}
-        with patch.object(FirebaseApplication, 'get', return_value=extData) as mock_FirebaseApplication:
+                   "remote_user" : "root"}
+        inventory = {"name": "host1",
+                     "group": "group1",
+                     "ansible_ssh_host": "host1",
+                     "ansible_ssh_user": "root"}
+        with patch.object(FirebaseApplication, 'get', side_effect=[extData, inventory]) as mock_FirebaseApplication:
             with patch.object(FirebaseApplication, 'patch', return_value=None) as mock_FirebaseApplication_post:
                 mock_FirebaseAuthentication = FirebaseAuthentication("secret", True, True)
                 mock_FirebaseAuthentication.__main__ = MagicMock(return_value="myauth")
